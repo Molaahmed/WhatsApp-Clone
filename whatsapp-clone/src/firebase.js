0b17c62d467f5596208f727e39a554fafe0d4247
Redzhep <redjep.molaahmed.rm@gmail.com>
2021-02-02 11:32:06 +0100

    Created the side bar design

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAUNzjRdvYKX2vCud4edWe4BPd2p1Mor18",
+    authDomain: "whats-app-clone-54a5c.firebaseapp.com",
+    projectId: "whats-app-clone-54a5c",
+    storageBucket: "whats-app-clone-54a5c.appspot.com",
+    messagingSenderId: "807298460574",
+    appId: "1:807298460574:web:11f9ab249ebb999f8c448f",
+    measurementId: "G-N3TD1GCVDV"
+  };
