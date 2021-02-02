05038488ba065156af6e1c265202495b747243b1
Redzhep <redjep.molaahmed.rm@gmail.com>
2021-02-02 11:52:14 +0100

    created a sidebar search only the design

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -3,7 +3,7 @@ import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-
+import { SearchOutlined } from "@material-ui/icons";
 
 import './Sidebar.css';
 
@@ -25,15 +25,24 @@ function Sidebar() {
                     </IconButton>
 
                 </div>
-
-
             </div>
+
+
             <div className="sidebar__search">
 
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat!" type="text" />
+                </div>
+
             </div>
+
+
             <div className="sidebar__header">
 
             </div>
+
+
             <div className="sidebar__chats">
 
             </div>
